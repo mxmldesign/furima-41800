1964f8d39478a97c167159662020413fa38a43c5
mxmldesign <nesignsprivate@gmail.com>
2024-12-09 17:57:46 +0900

    viewファイルの画像添付以外のフォームの編集とActive Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,16 @@ class ItemsController < ApplicationController
   def index
     @items = Item.order('created_at DESC')
   end
-end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:title, :description, :category_id, :condition_id, :shipping_id, :prefecture_id, :delivering_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+en,
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one :order
+  has_one_attached :image
 
   validates :title, presence: true
   validates :description, presence: true
