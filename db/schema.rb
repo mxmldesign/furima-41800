120cb3b0d69efdab474845a1e8c0df913bef9959
mxmldesign <nesignsprivate@gmail.com>
2024-12-09 17:33:49 +0900

    itemモデルとマイグレーション(Active_Hash)と投稿画面への遷移

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_05_052825) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_09_073624) do
+  create_table "items", charset: "utf8mb3", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "delivering_day_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -29,4 +43,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_05_052825) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
