ecdb45dbf56eec68bea33cee7acdbae7fc2ef14f
mxmldesign <nesignsprivate@gmail.com>
2024-12-10 18:35:27 +0900

    一部微修正とjavascriptで価格の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,12 +8,18 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
-  def message_params
-    params.require(:item).permit(:title, :description, :category_id, :condition_id, :shipping_id, :prefecture_id,
+  def item_params
+    params.require(:item).permit(:title, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
                                  :delivering_day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
