f0ec473ead166c9174cfbaea46509a29c493f62b
mxmldesign <nesignsprivate@gmail.com>
2024-12-12 11:28:05 +0900

    編集ページへのリンク付けとログインの有無などの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -21,10 +22,24 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:title, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
                                  :delivering_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    return if @item.user == current_user
+
+    redirect_to action: :index
   end
 end
