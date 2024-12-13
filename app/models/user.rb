b945e2cefedd466457828735ac7715e28813ce84
mxmldesign <nesignsprivate@gmail.com>
2024-12-13 15:52:32 +0900

    first commit, formオブジェクト

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,28 +7,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :password,
-            format: { with: /\A(?=.*[a-z])(?=.*\d)[a-z\d]+\z/i }
-  validates :surname_in_kanji,
-            presence: true,
-            format: {
-              with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
-            }
-  validates :first_name_in_kanji,
-            presence: true,
-            format: {
-              with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
-            }
-  validates :surname_in_kana,
-            presence: true,
-            format: {
-              with: /\A[ァ-ヶー]+\z/
-            }
-  validates :first_name_in_kana,
-            presence: true,
-            format: {
-              with: /\A[ァ-ヶー]+\z/
-            }
-  validates :date_of_birth, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :password,
+              format: { with: /\A(?=.*[a-z])(?=.*\d)[a-z\d]+\z/i }
+    validates :surname_in_kanji,
+              format: {
+                with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+              }
+    validates :first_name_in_kanji,
+              format: {
+                with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+              }
+    validates :surname_in_kana,
+              format: {
+                with: /\A[ァ-ヶー]+\z/
+              }
+    validates :first_name_in_kana,
+              format: {
+                with: /\A[ァ-ヶー]+\z/
+              }
+    validates :date_of_birth, presence: true
+  end
 end
