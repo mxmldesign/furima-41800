71bc59b09b6b6af54d6bbbf4202fb9705022a00d
mxmldesign <nesignsprivate@gmail.com>
2024-12-18 17:39:21 +0900

    プルダウン修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,13 +2,13 @@ class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: 'メンズ' },
-    { id: 3, name: 'ウィメンズ' },
+    { id: 3, name: 'レディース' },
     { id: 4, name: 'ベビー・キッズ' },
-    { id: 5, name: 'インテリア・雑貨' },
+    { id: 5, name: 'インテリア・住まい・小物' },
     { id: 6, name: '本・音楽・ゲーム' },
-    { id: 7, name: 'ホビー・玩具' },
-    { id: 8, name: '電化製品' },
-    { id: 9, name: 'スポーツ' },
+    { id: 7, name: 'おもちゃ・ホビー・グッズ' },
+    { id: 8, name: '家電・スマホ・カメラ' },
+    { id: 9, name: 'スポーツ・レジャー' },
     { id: 10, name: 'ハンドメイド' },
     { id: 11, name: 'その他' }
   ]
